4fa66bb7ce495c491896d5f115787595543a9d9d
Alvaro Saurin <alvaro.saurin@gmail.com>
2021-04-30 12:21:55 +0200

    Updated to latest version of k3d
    Signed-off-by: Alvaro Saurin <alvaro.saurin@gmail.com>

--- providers/k3d.sh
+++ providers/k3d.sh
@@ -18,9 +18,11 @@ K3D_EXE="k3d"
 
 K3D_INSTALL_EXE="$HOME/bin/k3d"
 
-K3D_URL="https://github.com/rancher/k3d/releases/download/v1.7.0/k3d-linux-amd64"
+K3D_INST_URL="https://raw.githubusercontent.com/rancher/k3d/main/install.sh"
 
 K3D_CLUSTER_NAME="${CLUSTER_NAME:-cluster-$user-$num}"
+
+K3D_KUBECONFIG="/tmp/k3d-kubeconfig-${K3D_CLUSTER_NAME}"
 
 K3D_NETWORK_NAME="k3d-$K3D_CLUSTER_NAME"
 
@@ -36,7 +38,7 @@ K3D_NUM_WORKERS=0
 
 K3D_EXTRA_ARGS="${K3D_EXTRA_ARGS:-}"
 
-K3D_ARGS="--wait=60 --name=${K3D_CLUSTER_NAME} --api-port ${K3D_API_PORT} --enable-registry --registry-name ${K3D_REGISTRY_NAME} --registry-port ${K3D_REGISTRY_PORT} ${K3D_EXTRA_ARGS}"
+K3D_ARGS="--wait --api-port ${K3D_API_PORT} --registry-use ${K3D_REGISTRY_NAME} ${K3D_EXTRA_ARGS}"
 
 #########################################################################################
 
@@ -44,21 +46,20 @@ export PATH=$PATH:$(dirname $K3D_INSTALL_EXE)
 
 [ -n "$CLUSTER_SIZE" ] && K3D_NUM_WORKERS=$((CLUSTER_SIZE - 1))
 
-get_kubeconfig() {
-    $K3D_EXE get-kubeconfig --name="$K3D_CLUSTER_NAME" 2>/dev/null
-}
-
 get_k3d_server_ip() {
-    local cont="k3d-$K3D_CLUSTER_NAME-server"
+    local cont="k3d-$K3D_CLUSTER_NAME-server-0"
     docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $cont
 }
 
-check_k3d_cluster_exists() {
-    $K3D_EXE list 2>/dev/null | grep -q "$K3D_CLUSTER_NAME"
+check_registry_exists() {
+    $K3D_EXE registry list | grep -q "$K3D_REGISTRY_NAME" >/dev/null 2>&1
 }
 
-# replace the IP in the kubeconfig (127.0.0.1 or localhost) by the real IP of the container,
-# so we can connect from the Ambassador builder container
+check_k3d_cluster_exists() {
+    $K3D_EXE cluster list 2>/dev/null | grep -q "$K3D_CLUSTER_NAME" >/dev/null 2>&1
+}
+
+# replace the IP in the kubeconfig (127.0.0.1 or localhost) by the real IP of the container
 replace_ip_kubeconfig() {
     local kc="$1"
     local current_ip=$(get_k3d_server_ip)
@@ -69,20 +70,31 @@ replace_ip_kubeconfig() {
     done
 }
 
-create_cluster() {
-    info "Creating k3d cluster $K3D_CLUSTER_NAME..."
-    $K3D_EXE create --workers $K3D_NUM_WORKERS --server-arg '--no-deploy=traefik' $K3D_ARGS
-    sleep 3
-    kc=$(get_kubeconfig)
-    [ -n "$kc" ] || abort "could not obtain a valid KUBECONFIG from k3d"
+create_registry() {
+    if check_registry_exists ; then
+        info "k3d registry alreay exists"
+    else
+        info "Creating k3d registry..."
+        $K3D_EXE registry create "$K3D_REGISTRY_NAME" --port $K3D_REGISTRY_PORT || abort
+    fi
+}
 
-    replace_ip_kubeconfig "$kc"
+create_cluster() {
+    create_registry || abort "could not create registry"
+
+    info "Creating k3d cluster $K3D_CLUSTER_NAME..."
+    KUBECONFIG="$K3D_KUBECONFIG" $K3D_EXE cluster create --agents $K3D_NUM_WORKERS $K3D_ARGS "$K3D_CLUSTER_NAME" || abort
+    sleep 3
+
+    # replace_ip_kubeconfig "$K3D_KUBECONFIG"
 
     info "Showing some k3d cluster info:"
-    kubectl --kubeconfig="$kc" cluster-info
+    kubectl --kubeconfig="$K3D_KUBECONFIG" cluster-info || abort
 }
 
 #########################################################################################
+
+[ -n "$1" ] || abort "no command provided"
 
 case $1 in
 #
@@ -92,10 +104,13 @@ setup)
     if ! command_exists $K3D_EXE; then
         info "Installing k3d"
 
-        curl -Lo ./k3d "$K3D_URL" || abort "could not download K3D from $K3D_URL"
-        chmod +x ./k3d
-        mkdir -p "$(dirname $K3D_INSTALL_EXE)"
-        mv ./k3d $K3D_INSTALL_EXE
+        curl -s "$K3D_INST_URL" | bash
+        [ $? -eq 0 ] || abort "could not download K3D from $K3D_INST_URL"
+
+        # chmod +x ./k3d
+        # mkdir -p "$(dirname $K3D_INSTALL_EXE)"
+        # mv ./k3d $K3D_INSTALL_EXE
+
         command_exists $K3D_EXE || abort "could not install k3d"
     else
         info "k3d seems to be installed"
@@ -142,7 +157,7 @@ create)
     if ! command_exists k3d; then
         warn "No k3d command found. Install k3d or use a different CLUSTER_PROVIDER."
         info "You can manually install k3d with:"
-        info "curl -Lo /tmp/k3d $K3D_URL ; chmod 755 /tmp/k3d ; sudo cp -f /tmp/k3d /usr/local/bin/"
+        info "curl -s $K3D_INST_URL | bash"
         abort "no k3d executable found"
     fi
 
@@ -155,27 +170,21 @@ create)
     ;;
 
 delete)
-    for ancestor in builder "$K3D_REGISTRY:aes"; do
-        cid="$(docker ps --filter ancestor=$ancestor -q)"
-        if [ -n "$cid" ]; then
-            info "Stopping container CID:$cid"
-            docker stop "$cid" 2>/dev/null
-        fi
-    done
-
     info "Destroying k3d cluster $K3D_CLUSTER_NAME..."
-    $K3D_EXE delete --name="$K3D_CLUSTER_NAME" 2>/dev/null || /bin/true
+    KUBECONFIG=$K3D_KUBECONFIG $K3D_EXE cluster delete "$K3D_CLUSTER_NAME" 2>/dev/null || /bin/true
+    rm -f "$K3D_KUBECONFIG"
     ;;
 
 #
 # create and destroy the registry
-# in the k3d case, the registry is associated to the cluster
 #
 create-registry)
-    if check_k3d_cluster_exists; then
-        info "A cluster $K3D_CLUSTER_NAME exists: nothing to do..."
-    else
-        create_cluster
+    create_registry
+    ;;
+
+delete-registry)
+    if check_registry_exists ; then
+        $K3D_EXE registry delete "$K3D_REGISTRY_NAME"
     fi
     ;;
 
@@ -190,19 +199,16 @@ exists)
 # get the environment vars
 #
 get-env)
-    export_env "DEV_REGISTRY" "${K3D_REGISTRY}"
+    export_env "DEV_REGISTRY" "k3d-${K3D_REGISTRY}"
     export_env "DOCKER_NETWORK" "${K3D_NETWORK_NAME}"
 
-    kc=$(get_kubeconfig)
-    if [ -n "$kc" ]; then
-        export_env "DEV_KUBECONFIG" "${kc}"
-        export_env "KUBECONFIG" "${kc}"
-    fi
+    export_env "DEV_KUBECONFIG" "$K3D_KUBECONFIG"
+    export_env "KUBECONFIG" "$K3D_KUBECONFIG"
 
     export_env "CLUSTER_NAME" "$K3D_CLUSTER_NAME"
     export_env "CLUSTER_SIZE" "$((K3D_NUM_WORKERS + 1))"
-    export_env "CLUSTER_MACHINE="
-    export_env "CLUSTER_REGION="
+    export_env "CLUSTER_MACHINE"
+    export_env "CLUSTER_REGION"
 
     # k3d-specific vars
     export_env "K3D_CLUSTER_NAME" "$K3D_CLUSTER_NAME"
@@ -211,7 +217,7 @@ get-env)
     ;;
 
 *)
-    info "'$1' ignored for $CLUSTER_PROVIDER"
+    info "command '$1' ignored for $CLUSTER_PROVIDER"
     ;;
 
 esac
